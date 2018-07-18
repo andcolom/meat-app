c84016289bac9cd85ee9d73164fc52b7ad5250b9
Andre <andcolom@yahoo.com.br>
2018-07-18 00:51:24 -0300

    Meat app até aula 74

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -4,6 +4,8 @@ import { OrderService } from './order.service';
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
 import { Order, OrderItem } from './order.model';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, AbstractControl }  from '@angular/forms';
+
 
 @Component({
   selector: 'mt-order',
@@ -12,10 +14,41 @@ import { Router } from '@angular/router';
 })
 export class OrderComponent implements OnInit {
 
-  constructor(private service:OrderService, private router: Router) { }
+  constructor(private service:OrderService
+    , private router: Router
+    , private builder: FormBuilder) { }
 
   ngOnInit() {
+    this.orderForm = this.builder.group(
+      {
+        name: this.builder.control("", [Validators.required,Validators.minLength(5)]),
+        email:this.builder.control("", [Validators.required,Validators.pattern(this.emailPattern)]),
+        emailConfirmation:this.builder.control("",[Validators.required,Validators.pattern(this.emailPattern)]),
+        address:this.builder.control("",[Validators.required,Validators.minLength(5)]),
+        number:this.builder.control("",[Validators.required,Validators.pattern(this.numberPattern)]),
+        optionalAddress:this.builder.control(""),
+        paymentOption:this.builder.control("",[Validators.required]),
+      },
+      { validator: OrderComponent.equalsTo}
+    )
   }
+
+  static equalsTo(group: AbstractControl) : {[key: string]:boolean} {
+    const email = group.get("email")
+    const emailConfirmation = group.get("emailConfirmation")
+    if ( !email || !emailConfirmation){
+      return undefined
+    }
+    if(emailConfirmation.value != email.value){
+      return {"emailsNotMatch":true}
+    }
+
+    return undefined;
+  }
+
+  emailPattern  = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  numberPattern = /^[0-9]*$/
+  orderForm: FormGroup
 
   delivery:number = 8.00
 
--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, ContentChild, AfterContentInit } from '@angular/core';
-import { NgModel } from '@angular/forms';
+import { NgModel, FormControlName } from '@angular/forms';
 
 @Component({
   selector: 'mt-input-container',
@@ -9,13 +9,14 @@ import { NgModel } from '@angular/forms';
 export class InputComponent implements OnInit, AfterContentInit {
 
   ngAfterContentInit(): void {
-    this.input = this.model
+    this.input = this.model || this.control
     if(this.input === undefined)
       throw new Error("Componente precisa ser utilizado com diretiva.");
   }
   constructor() { }
 
   ngOnInit() {
+  
   }
 
   input: any
@@ -23,6 +24,7 @@ export class InputComponent implements OnInit, AfterContentInit {
   @Input() errorMessage:string
 
   @ContentChild(NgModel) model : NgModel
+  @ContentChild(FormControlName) control : FormControlName
 
   hasSuccess (): boolean{
     return this.input.valid && (this.input.dirty || this.input.touched)
